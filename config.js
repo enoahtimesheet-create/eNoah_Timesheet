adc19c92dfafcb92c0df467a60515d4fc1e90131
enoahtimesheet-create <enoahtimesheet@gmail.com>
2026-02-13 01:05:51 +0530

    changed the leave day type 1 into 0.5 for the 4hrs

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const CONFIG = {
     // 1. Create a Google Apps Script project (instructions in SETUP.md)
     // 2. Deploy as Web App
     // 3. Copy the deployment URL here
-    GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxYltsrFU4TPRkYHWowVpYUTWf4eK2srhoxHm6px3a7NPP3msvb5NPEoKlGHB_ehwk17Q/exec',
+    GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyCWELoMTuEHYRubj9Ie8nLnu0ylduB386qAfTe0044Mo63Cb2zKSmSDIkRzHZ8Bre6xg/exec',
     
     // Email Configuration for OTP
     // The Google Apps Script will handle sending emails
